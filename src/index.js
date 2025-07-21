fe08ee0d1d53ecc128361a5cd0f4e3c1ada909c2
MadMilliner <jasonapeterson@gmail.com>
2025-07-21 10:35:23 -0700

    It works a little now, getting somewhere

--- src/index.js
+++ src/index.js
@@ -8,46 +8,47 @@ function setTheme() {
 };
 document.querySelector('.theme-toggle').addEventListener('click', setTheme);
 
-if (process.env.NODE_ENV !== 'production') {
 
-   console.log('Looks like we are in development mode!');
+const todos = [];
+window.todos = todos; 
 
- }
-import { compareAsc, format } from "date-fns";
+class createTodo {
+  constructor(title, description, dueDate) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = "medium";
+    this.categories = [];
+    this.complete = false;
+    this.id = crypto.randomUUID();
+    this.addTodos();
+  }
+  addTodos() {
+    todos.push(this);
+  }
+}
 
-format(new Date(2014, 1, 11), "yyyy-MM-dd");
-//=> '2014-02-11'
+window.createTodo = createTodo;
+// const createTodo = (title, description, dueDate) => {
+//   const todo = {
+//     title: title,
+//     description: description,
+//     dueDate: dueDate,
+//     priority: "medium",
+//     categories: [],
+//     complete: false,
+//     id: crypto.randomUUID(),
+//   };
+//   todos.push(todo);
+//   return todo;
+// };
 
-const dates = [
-  new Date(1995, 6, 2),
-  new Date(1987, 1, 11),
-  new Date(1989, 6, 10),
-];
-dates.sort(compareAsc);
-//=> [
-//   Wed Feb 11 1987 00:00:00,
-//   Mon Jul 10 1989 00:00:00,
-//   Sun Jul 02 1995 00:00:00
-// ]
-
-let todos = [];
-
-function createTodo(title, description, dueDate) {
-  let todo = {};
-  todo.title = title;
-  todo.description = description;
-  todo.dueDate = dueDate;
-  todo.priority = "medium";
-  todo.tags = [];
-  todo.complete = false;
-  todo.id = crypto.randomUUID();
-  todos.push(todo);
-  return todo;
-};
-
-let todo1 = createTodo("Eat lunch", "Eat some lunch", "2025-07-17");
+let testTodo = new createTodo("Eat lunch", "Eat some lunch", "2025-07-17");
+console.log(testTodo);
+console.log(todos);
 
 let tags = [];
+window.tags = tags;
 
 function createTag(title) {
   let tag = [];
@@ -61,40 +62,83 @@ createTag("Work");
 createTag("Play");
 
 const pageMain = {
-  getDom: display = document.getElementById("display"),
   mainDisplay: function() {
-    for (i = 0; i = todos.length; i++)
-      display.innerHTML+=`${this.title}, ${this.description}, ${this.dueDate}`
+    todos.forEach((todo) => {
+      display = document.getElementById("display");
+      var todoCard = document.createElement("div");
+      todoCard.setAttribute("id", `${todo.id}`);
+      todoCard.innerHTML = `
+        <p>Task: <span class="todoTitle">${todo.title}</span></p>
+        <p>Description: ${todo.description}</p>
+        <p>Due Date: ${todo.dueDate}</p>
+        <p>Priority: ${todo.priority}</p>
+        <p>Categories: ${todo.categories}</p>
+        <p><input type="checkbox" class="complete" id="${todo.id}">`;
+      display.appendChild(todoCard);
+    });
   },
+  // return {mainDisplay}
 };
+
+pageMain.mainDisplay();
 
 const userInteract = {
   markComplete: function() {
-    this.complete = !this.complete
+    var checkbox = `${this.complete}`;
+    this.complete = !this.complete;
   },
   addTask: function() {
     let title = document.getElementById("taskTitle").value;
     let description = document.getElementById("taskDescription").value;
     let dueDate = document.getElementById("taskDateDue");
-    createTodo(title, description, dueDate);
+    new createTodo(title, description, dueDate);
+    pageMain.mainDisplay();
     document.getElementById("addTask-form").reset();
+    document.getElementById("addTaskDisplay").style.display = "none";
   },
   addCategory: function(tag) {
-    tag = prompt("What is the category name");
     createTag(tag);
+  },
+  addTaskPopUp: function() {
+    document.getElementById("addTaskDisplay").style.display = "block";
+    document.getElementById("addThatTask").addEventListener("click", userInteract.addTask);
+    document.getElementById("closeAddTaskDisplay").addEventListener("click", function() {
+    document.getElementById("addTaskDisplay").style.display = "none";
+  })
+},
+  init: function() {
+    this.markComplete;
+    this.addTask;
+    this.addCategory;
+    this.addTaskPopUp;
+    this.addTaskcancelButton;
+  },
+};
+
+userInteract.init();
+
+const pageElements = {
+  // addTaskButton: document.addEventListener("click", userInteract.addTask),
+  addCategoryButton: document.addEventListener("click", userInteract.addCategory),
+  eventListenerAddTask: document.getElementById("addTodo").addEventListener("click", userInteract.addTaskPopUp),
+  eventListenerAddCategory: document.getElementById("addCategory").addEventListener("click", function() {
+    const tag = prompt("What is the category name");
+  if (tag) { // Check if user didn't cancel
+    userInteract.addCategory(tag);
+  }
+}),
+  init: function() {
+    // this.addTaskButton;
+    this.addCategoryButton;
+    this.addTaskPopUp;
+    this.eventListenerAddTask;
+    this.eventListenerAddCategory;
   }
 };
 
-const pageElements = {
-  addTaskButton: document.addEventListener("click", userInteract.addTask()),
-  addCategoryButtoon: document.addEventListener("click", userInteract.addCategory()),
-  addTaskPopUp: function() {
-    let addTaskForm = document.getElementById("addTask-display");
-    addTaskForm.dialog( "open" );
-    document.getElementById("addThatTask").addEventListener("click", userInteract.addTask())
-  },
-  eventListenerAddTask: document.getElementById("addTodo").addEventListener("click", pageElements.addTaskPopUp())
-};
+pageElements.init();
+
+
 
 
 
